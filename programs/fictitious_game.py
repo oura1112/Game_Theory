69cbd8a0e7bb46404d7dcf91f1c74e5fa19efb6b
oura1112 <59088115+oura1112@users.noreply.github.com>
2020-05-21 23:49:07 +0900

    0521

---
+++ programs/fictitious_game.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 13:56:08 2020
+
+@author: ryohe
+"""
+
+import math
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import copy
+
+class fictitious_game():
+    
+    def __init__(self, player_num, action_num):
+        self.emp_dist_ndiv = [ [0]*action_num for i in range(player_num) ]
+        #self.actions_list = [ [action for action in range(action_num)] for player in player_num ]
+        self.actions = [i for i in range(action_num)]
+        self.players = [i for i in range(player_num)]
+        self.player_num = player_num
+        self.action_num = action_num
+        self.dist_history = [[[0] for j in range(self.action_num)] for i in range(self.player_num)]
+        
+        
+    def Utility(self,actions):
+        one = np.zeros(3)
+        two = one + 1 
+        #three = two + 1
+        #print(two==actions)
+        if all(one == actions):
+            return 1
+        elif all(two == actions):
+            return 2
+        #elif all(three == actions):
+         #   return 3
+        else:
+            return 0
+    
+    def empirical_dist(self, episode, player, action_prev):
+        
+        #print(1)
+        if action_prev == -1:
+            return self.emp_dist_ndiv
+        else:
+            #print(player)
+            #print(action_prev)
+            self.emp_dist_ndiv[player][action_prev] += 1
+            #print(self.emp_dist_ndiv)
+            empirical_dist = np.array(self.emp_dist_ndiv)/(episode)
+            return empirical_dist
+    
+    def policy_fictitious(self, episode, player_i, action_prev):
+        players_except = [ j for j in self.players if j != player_i ]
+        q = 1
+        util = 0
+        utils = []
+        action_list = []
+        
+        empirical_dist = self.empirical_dist(episode, player_i, action_prev)
+        #print(empirical_dist)
+        #print(np.array(self.emp_dist_ndiv)/1)
+                
+        for action in self.actions:
+            #actions_except = [ j for j in self.actions if j != action]
+            action_list_all = []
+            
+            for j in players_except:
+                q *= empirical_dist[j][action]
+            
+            #print(q)
+            #残り2プレイヤー分の行動リストの生成
+            for action1 in self.actions:
+                for action2 in self.actions:
+                    action_list.append(action1)
+                    action_list.append(action2)
+                    action_list.insert(player_i, action)
+                    action_list_all.append(action_list)
+                    
+                    action_list = []
+                    
+            for actions in action_list_all:      
+                util += self.Utility(actions)
+                
+            utils.append(util)
+            #print(len(utils))
+            
+            util = 0
+            q = 1
+        #utils = np.array(utils)
+        return np.argmax(utils)
+    
+    def policy_log_linear(self, player, actions_prev, tau=10):
+        action_probs = []
+
+        actions_prev_except = copy.copy(actions_prev)
+        for action in self.actions:
+            del actions_prev_except[player]
+            actions_prev_except.insert(player, action)
+            util = self.Utility(actions_prev_except)
+            action_probs.append( math.exp( (1/tau) * util) )
+                
+        action_probs = np.array(action_probs)
+        action_probs = action_probs/sum(action_probs)
+        
+        action =np.random.choice(self.actions, 1, list(action_probs))
+        return action, action_probs
+    
+    def learn_fictitious(self, episode_num = 30, initial_action = [0,0,0]):
+        
+        #print(len(action_history))
+        #dist = np.array(dist_history)
+        #print(dist.shape)
+        
+        #emp_dist = copy.deepcopy(self.emp_dist_ndiv)
+        #dist_history.append(np.array(emp_dist))
+        
+        count = 0
+        
+        episode_axis = [0]
+        
+        action_prev = initial_action
+        action = action_prev
+        for episode in range(episode_num):
+                
+            for player in self.players:
+                #print(action_prev[player])
+                
+                action[player] = self.policy_fictitious(episode, player, action_prev[player])  
+                #print(action)
+                action_prev[player] = action[player]
+                #empirical_dist = self.empirical_dist(episode+1, player, action_prev)
+        
+            if episode % 2 == 0:
+                episode_axis.append(episode)
+                emp_dist = np.array(self.emp_dist_ndiv) / (episode+1)
+                for player_i in self.players:
+                    for action_i in self.actions:
+                        count += 1
+                        #print(player_i)
+                        #print(action_i)
+                        #print(emp_dist[player][action_i])
+                        self.dist_history[player_i][action_i].append(emp_dist[player][action_i])
+                        #print(len(self.dist_history[player_i][action_i]))
+                
+        empirical_dist = np.array(self.emp_dist_ndiv) / episode_num
+        #print(empirical_dist)
+        #print(2)
+        dist = np.array(self.dist_history)
+        #print(dist.shape)
+        #print(count)
+        
+        
+        plt.style.use('default')
+        sns.set()
+        sns.set_style('whitegrid')
+        sns.set_palette('gray')
+        
+        fig = plt.figure()
+        player1 = fig.add_subplot(3, 1, 1)
+        player2 = fig.add_subplot(3, 1, 2)
+        player3 = fig.add_subplot(3, 1, 3)
+        
+        #print(len(self.dist_history[1][1]))
+        player1.plot(episode_axis, self.dist_history[0][1], color="blue")
+        player2.plot(episode_axis, self.dist_history[1][1], color="red")
+        player3.plot(episode_axis, self.dist_history[2][1], color="green")
+        
+        plt.show()
+        
+        fig = plt.figure()
+        player1 = fig.add_subplot(3, 1, 1)
+        player2 = fig.add_subplot(3, 1, 2)
+        player3 = fig.add_subplot(3, 1, 3)
+        
+        #print(len(self.dist_history[1][1]))
+        player1.plot(episode_axis, self.dist_history[0][0], color="blue")
+        player2.plot(episode_axis, self.dist_history[1][0], color="red")
+        player3.plot(episode_axis, self.dist_history[2][0], color="green")
+        
+        plt.show()
+        
+    def learn_log_linear(self, episode_num = 100):
+        
+        count = 1
+        
+        episode_axis = [0]
+        
+        action_prev = [0,0,0]
+        action = action_prev
+        for episode in range(episode_num):
+                
+            for player in self.players:               
+                action[player], action_probs = self.policy_log_linear(player, action_prev, tau = 10000)
+                #print(action[player])
+                action_prev[player] = action[player]
+        
+                if episode % 2 == 0:
+                    if player == 0:
+                        episode_axis.append(episode)
+                    for action_i in self.actions:
+                        self.dist_history[player][action_i].append(action_probs[action_i])
+            
+        empirical_dist = np.array(self.emp_dist_ndiv) / episode_num
+        dist = np.array(self.dist_history)       
+        
+        plt.style.use('default')
+        sns.set()
+        sns.set_style('whitegrid')
+        sns.set_palette('gray')
+        
+        fig = plt.figure()
+        player1 = fig.add_subplot(3, 1, 1)
+        player2 = fig.add_subplot(3, 1, 2)
+        player3 = fig.add_subplot(3, 1, 3)
+        
+        player1.plot(episode_axis, self.dist_history[0][1], color="blue")
+        player2.plot(episode_axis, self.dist_history[1][1], color="red")
+        player3.plot(episode_axis, self.dist_history[2][1], color="green")
+        
+        plt.show()
+        
+        fig = plt.figure()
+        player1 = fig.add_subplot(3, 1, 1)
+        player2 = fig.add_subplot(3, 1, 2)
+        player3 = fig.add_subplot(3, 1, 3)
+        
+        #print(len(self.dist_history[1][1]))
+        player1.plot(episode_axis, self.dist_history[0][0], color="blue")
+        player2.plot(episode_axis, self.dist_history[1][0], color="red")
+        player3.plot(episode_axis, self.dist_history[2][0], color="green")
+        
+        plt.show()
+
+if __name__ == "__main__":        
+    f_game = fictitious_game(player_num = 3, action_num = 2)
+    #f_game.learn_fictitious()
+    f_game.learn_log_linear()         
+                
+#util = Utility([2,2,2])
+#print(util)
